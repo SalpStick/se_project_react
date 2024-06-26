0b153aa24385fcf9d979cae9b083c845cccfaf5d
SalpStick <43050248+SalpStick@users.noreply.github.com>
2024-06-26 14:00:23 -0400

    feat/ Added Clothes section

---
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -0,0 +1,31 @@
+import ItemCard from "../ItemCard/ItemCard";
+import "./ClothesSection.css";
+
+function ClothesSection({ handleCardClick, clothingItems, handleAddClick }) {
+  return (
+    <div className="clothes-section">
+      <div className="clothes__button">
+        <p className="clothes-section_your-itemsText">Your items</p>
+        <button
+          onClick={handleAddClick}
+          type="button"
+          className="clothing-section_addBtn"
+        >
+          + Add New
+        </button>
+      </div>
+      <ul className="clothes-section__items">
+        {clothingItems.map((item) => {
+          return (
+            <ItemCard
+              key={item._id}
+              item={item}
+              onCardClick={handleCardClick}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+export default ClothesSection;
